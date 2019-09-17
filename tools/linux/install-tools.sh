ae3479001faff5bfeb83eca0d06619a989c1c176
Henry <zat_xorg@hotmail.com>
2019-09-17 16:39:22 -0300

    + pacman arm-none-eabi-*

--- tools/linux/install-tools.sh
+++ tools/linux/install-tools.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo gpasswd -a $USER uucp
-sudo pacman -S gconf git --noconfirm --need
+sudo pacman -S gconf git arm-none-eabi-newlib arm-none-eabi-gcc arm-none-eabi-gdb openocd eclipse-cpp moserial  --noconfirm --need
 sudo pacman -U pkg/*.xz --noconfirm
