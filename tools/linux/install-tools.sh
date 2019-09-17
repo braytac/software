59afb4b648980f2dc04b41783f2f427ae85cb629
Henry <zat_xorg@hotmail.com>
2019-09-17 16:32:39 -0300

    Update install-tools.sh

--- tools/linux/install-tools.sh
+++ tools/linux/install-tools.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gksudo gpasswd -a $USER uucp
-gksudo pacman -S gconf git --noconfirm --need
-gksudo pacman -U pkg/*.xz --noconfirm
+sudo gpasswd -a $USER uucp
+sudo pacman -S gconf git --noconfirm --need
+sudo pacman -U pkg/*.xz --noconfirm
